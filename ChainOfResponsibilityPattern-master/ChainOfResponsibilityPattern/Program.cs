d95ffddb4df5109c82a4608b5eb7e087939fc853
Melih TAŞOĞLAN <melih.tasoglan@outlook.com>
2023-05-17 23:11:37 +0300

    ChainOfResponsibilityPattern

---
+++ ChainOfResponsibilityPattern-master/ChainOfResponsibilityPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ChainOfResponsibilityPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Setup Chain of Responsibility
+            Handler redHandler = new RedHandler();
+            Handler greenHandler = new GreenHandler();
+            Handler blueHandler = new BlueHandler();
+
+            redHandler.SetNextHandler(greenHandler);
+            greenHandler.SetNextHandler(blueHandler);
+
+
+            // Requests
+
+            Request request = null;
+
+            request = new Request("001", "Red");
+            redHandler.ProcessRequest(request);
+
+            request = new Request("002", "Green");
+            redHandler.ProcessRequest(request);
+
+            request = new Request("003", "Blue");
+            redHandler.ProcessRequest(request);
+
+            request = new Request("999", "White");
+            redHandler.ProcessRequest(request);
+        }
+    }
+}
